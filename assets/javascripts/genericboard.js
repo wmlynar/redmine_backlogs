d958732267a3b755e64fd6352e5e5f247e17e6da
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2014-03-08 06:08:15 +0100

    use floating table header for board

--- assets/javascripts/genericboard.js
+++ assets/javascripts/genericboard.js
@@ -7,4 +7,14 @@ RB.$(function() {
         document.location.href = RB.urlFor('genericboards', {id: this.value});
     });
 
+    //fixup width of cells #FIXME make this configurable like taskboard
+    var _width = 240;
+    RB.$(".swimlane").width(_width).css('min-width', _width);
+
+    /* make taskbord swimlane header floating */
+    RB.$("#board_header").verticalFix({
+        delay: 50
+    });
+
+
 });
